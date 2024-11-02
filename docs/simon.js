19a0fa4f292615d1686fd35ab3f259d0fbc4297f
rodrigoARP <rodrigo.adrian.rodriguezpuerto@gmail.com>
2024-11-02 13:04:44 -0600

    Botón inicio y mensaje fallo

--- docs/simon.js
+++ docs/simon.js
@@ -3,38 +3,94 @@ const btnDos = document.getElementById("dos");
 const btnTres = document.getElementById("tres");
 const btnCuatro = document.getElementById("cuatro");
 
+let botonPresionado = false;
 let clickingOrder = [];
+let index = 0;
+let arrayAleatorio = [];
 
-btnUno.onclick = () => tono(1174,btnClicked(1));
-btnDos.onclick = () => tono(1046,btnClicked(2));
-btnTres.onclick = () => tono(1318,btnClicked(3));
-btnCuatro.onclick = () => tono(1397.1,btnClicked(4));
+const n = 20;
+
+
+/* ------ Aqui creamos el patrón de números ------ */
+function patron(){
+    for (let i = 1; i < 21; i++) {
+        let numeroAleatorio = Math.floor(Math.random()*4+1); 
+        arrayAleatorio.push(numeroAleatorio);
+    }
+    console.log(arrayAleatorio);
+    
+    return arrayAleatorio;
+}
+/* ------ Aqui creamos el patrón de números ------ */
+
+function iniciarJuego() {
+    const boton = document.getElementById("botonInicio");
+    boton.style.display = "none";
+    botonPresionado = true;
+    activarBotones();
+    patron()
+}
+
+
+function activarBotones() {
+    if (!botonPresionado) return;
+
+    btnUno.onclick = () => manejarClick(1, 1174);
+    btnDos.onclick = () => manejarClick(2, 1046);
+    btnTres.onclick = () => manejarClick(3, 1318);
+    btnCuatro.onclick = () => manejarClick(4, 1397.1);
+}
+
+function manejarClick(num, frecuencia) {
+    tono(frecuencia, () => btnClicked(num));
+}
+
 
 function btnClicked(num) {
-    if (clickingOrder.length >= 20) {
-        clickingOrder = [];
-        clickingOrder.push(num);
-        console.log(clickingOrder);
+
+    clickingOrder.push(num);
+
+    if (num === arrayAleatorio[index]) {
+        index++;
+        if (index === arrayAleatorio.length) {
+            console.log("¡Completado!");
+        }
     } else {
-        clickingOrder.push(num);
-        console.log(clickingOrder);
+        console.log("Te equivocaste en la posición " + (1+index));
+        mostrarModalFallo();
+        clickingOrder = [];
+        index = 0;
     }
+
+    console.log("Clicking Order: ", clickingOrder);
 }
 
 function tono(frecuencia, callback) {
-    let audioCtx = new window.AudioContext;
+    let audioCtx = new window.AudioContext();
 
     let oscilador = audioCtx.createOscillator();
     let ganancia = audioCtx.createGain();
-    
+
     oscilador.connect(ganancia);
     ganancia.connect(audioCtx.destination);
-    
-    if (frecuencia){oscilador.frequency.value = frecuencia;} //frecuencia en Hz. Default: 440 Hz
-    if (callback){oscilador.onended = callback;} //llamar función después del tono
-    
+
+    if (frecuencia) oscilador.frequency.value = frecuencia;
+    if (callback) oscilador.onended = callback;
+
     oscilador.start(audioCtx.currentTime);
-    oscilador.stop(audioCtx.currentTime + (150 / 1000)); //duración en milisegundos. Default: 150 ms
+    oscilador.stop(audioCtx.currentTime + (150 / 1000));
 
     audioCtx.resume();
-};
+}
+
+function simon (){
+    
+}
+
+function mostrarModalFallo() {
+    mensajeFallo.style.display = "flex";
+}
+
+function reiniciarJuego() {
+    mensajeFallo.style.display = "none";
+}
